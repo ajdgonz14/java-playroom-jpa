86105b0854f29b11fd8379ff838bc2de35e59c40
ajdgonz14 <ajdgonz14@gmail.com>
2023-08-22 13:34:06 +0800

    initial commit

---
+++ src/test/java/com/learning/java/api/playroom/jpa/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.java.api.playroom.jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
